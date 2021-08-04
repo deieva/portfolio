900f3f1abbfa99a92deb24f3d15c7ef276015427
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-04 18:02:30 +0300

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -11,6 +11,9 @@ function Home() {
             move: {
                 enable: true
             },
+            color: {
+                value: ["#5bc0eb", "#fde74c", "#9bc53d", "#e55934", "#fa7921"],
+            },
         }
     });
 
