19e0956ad3ad29c9bf7df34c154b5bb511c56c14
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-05 19:39:35 +0300

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -88,16 +88,16 @@ function Home() {
 
     return (
         <div>
-            {/* <div id="tsparticles"></div> */}
+            <div id="tsparticles"></div>
 
-{/* <div className="login"/>
+<div className="login"/>
     <div className="container"/>
         <div className="login-container-wrapper clearfix"/>
             <div className="logo">
                 <i className="fa fa-sign-in"></i>
             </div>
             <div className="welcome"><strong>Welcome,</strong> please login</div>
-            <form className="form-horizontal login-form">
+            {/* <form className="form-horizontal login-form">
                 <div className="form-group relative">
                     <input id="login_username" className="form-control input-lg" type="email" placeholder="Username" required/>
                     <i className="fa fa-user"></i>
@@ -115,9 +115,7 @@ function Home() {
                 <div className="checkbox pull-right">
                     <label> <a className="forget" href="" title="forget">Forgot your password</a> </label>
                 </div>
-            </form>
-        </div> */}
-
+            </form> */}
         </div>
         
     
