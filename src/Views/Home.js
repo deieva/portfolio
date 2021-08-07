70b6fd661457c2f6be18664f2d99172b855a1463
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-07 22:13:14 +0300

    updates

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -65,7 +65,7 @@ function Home() {
                     }
                 },
                 bubble: {
-                    distance: 100,
+                    distance: 200,
                     size: 40,
                     duration: 2,
                     opacity: 0.8
@@ -87,12 +87,38 @@ function Home() {
     });
 
     return (
-       <div>
-           
-       </div>
-            
-        
-    
+        <div>
+            {/* <div id="tsparticles"></div> */}
+            {/* <div className="login"> */}
+                {/* <div className="container"> */}
+                    {/* <div className="login-container-wrapper clearfix">
+                        <div className="logo">
+                            <i className="fa fa-sign-in"></i>
+                        </div> */}
+                        {/* <div className="welcome"><strong>Welcome,</strong> please login</div> */}
+                        {/* <form className="form-horizontal login-form">
+                            <div className="form-group relative">
+                                <input id="login_username" className="form-control input-lg" type="email" placeholder="Username" required />
+                                <i className="fa fa-user"></i>
+                            </div>
+                            <div className="form-group relative password">
+                                <input id="login_password" className="form-control input-lg" type="password" placeholder="Password" required />
+                                <i className="fa fa-lock"></i>
+                            </div>
+                            <div className="form-group">
+                                <button type="submit" className="btn btn-success btn-lg btn-block">Login</button>
+                            </div>
+                            <div className="checkbox pull-left">
+                                <label><input type="checkbox" /> Remember</label>
+                            </div>
+                            <div className="checkbox pull-right">
+                                <label> <a className="forget" href="" title="forget">Forgot your password</a> </label>
+                            </div>
+                        </form> */}
+                    {/* </div> */}
+                {/* </div> */}
+            {/* </div> */}
+        </div>
     )
 }
 
