a92b51320e8d741c6daf319d6820d86d9638b6d7
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-08 23:11:05 +0300

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -88,15 +88,15 @@ function Home() {
 
     return (
         <div>
-            {/* <div id="tsparticles"></div> */}
-            {/* <div className="login"> */}
-                {/* <div className="container"> */}
-                    {/* <div className="login-container-wrapper clearfix">
+            <div id="tsparticles"></div>
+             <div className="login"> 
+                <div className="container">
+                    <div className="login-container-wrapper clearfix">
                         <div className="logo">
                             <i className="fa fa-sign-in"></i>
-                        </div> */}
-                        {/* <div className="welcome"><strong>Welcome,</strong> please login</div> */}
-                        {/* <form className="form-horizontal login-form">
+                        </div> 
+                        <div className="welcome"><strong>Welcome,</strong> please login</div> 
+                    <form className="form-horizontal login-form">
                             <div className="form-group relative">
                                 <input id="login_username" className="form-control input-lg" type="email" placeholder="Username" required />
                                 <i className="fa fa-user"></i>
@@ -114,10 +114,10 @@ function Home() {
                             <div className="checkbox pull-right">
                                 <label> <a className="forget" href="" title="forget">Forgot your password</a> </label>
                             </div>
-                        </form> */}
-                    {/* </div> */}
-                {/* </div> */}
-            {/* </div> */}
+                        </form> 
+                    </div> 
+                </div>
+            </div>
         </div>
     )
 }
